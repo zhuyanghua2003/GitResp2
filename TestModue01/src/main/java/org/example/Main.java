88aea1642f69064a049502e233635e4ae932e752
zhuyanghua <1191082340@qq.com>
2025-07-26 17:10:49 +0800

    二次进行数据的提交。。。。。

--- TestModue01/src/main/java/org/example/Main.java
+++ TestModue01/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String a="顾煜0821";
+        System.out.println(a);
     }
 }
